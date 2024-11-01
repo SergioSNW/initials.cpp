a38a5a6af3ac39bb97b76714e2ce3840005262ef
Sergio Santamaria Nora <83676008+SergioSNW@users.noreply.github.com>
2024-11-01 14:00:40 +0000

    Create initials.cpp

---
+++ initials.cpp
@@ -0,0 +1,19 @@
+// This initials.cpp program will display my initials and a
+// fun fact about myself is that I am resolved to game 
+// hahaha.
+
+#include <iostream>
+
+int main() {
+
+  // Code for displaying initials.
+std::cout << " SSS    SSS   N   N\n";
+std::cout << "S   S  S   S  NN  N\n";
+std::cout << "S      S      N N N\n";
+std::cout << " SSS    SSS   N  NN\n";
+std::cout << "    S      S  N   N\n";
+std::cout << "S   S  S   S  N   N\n";
+std::cout << " SSS    SSS   N   N\n";
+
+return 0;
+}
